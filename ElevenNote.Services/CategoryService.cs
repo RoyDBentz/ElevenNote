127de09d86145e14525501cbdd8daecf27504334
RoyDBentz <roydbentz@gmail.com>
2021-09-04 12:58:47 -0400

    Adding Category

---
+++ ElevenNote.Services/CategoryService.cs
@@ -0,0 +1,109 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class CategoryService
+    {
+        private readonly Guid _userId;
+        public CategoryService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateCategory(CategoryCreate model)
+        {
+            var entity =
+                new Category()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Categories.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CategoryListItem> GetCategories()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Categories
+                        .Where(c => c.OwnerId == _userId)
+                        .Select(
+                            c =>
+
+                                new CategoryListItem
+                                {
+                                    CategoryId = c.CategoryId,
+                                    Title = c.Title,
+                                    CreatedUtc = c.CreatedUtc
+                                }
+                         );
+                return query.ToArray();
+            }
+        }
+
+        public CategoryDetail GetCategoryById(int id)
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categories
+                        .Single(c => c.CategoryId && c.OwnerId == _userId);
+                return
+                    new CategoryDetail
+                    {
+                        CategoryId = entity.CategoryId,
+                        Title = entity.Title,
+                        Content = entity.Content,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc,
+                    };
+            }
+        }
+        public bool UpdateCategory(CategoryEdit model)
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categories
+                        .Single(c => c.CategoryId == model.CategoryId && c.OwnerId == _userId);
+
+                entity.Title = model.Title;
+                entity.Content = model.Content;
+                entity.ModifiedUtc = DateTimeOffset.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+
+            }
+        }
+
+        public bool DeleteCategory(int catergoryId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categories
+                        .Single(c => c.CategoryId == catergoryId && c.OwnerId == _userId);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+
+}
---
+++ ElevenNote.WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    public class CategoryController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            CategoryService categoryService = CreateCategoryService();
+            var category = categoryService.GetCategories();
+            return Ok(category);
+        }
+    }
+}
